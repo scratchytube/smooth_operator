abef6c074927e238725f617a07bc9e5248e05d7d
Brian Sahota <bpsahota@gmail.com>
2021-07-27 14:43:37 -0400

    hero almost done

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -8,6 +8,17 @@ const Hero = () => {
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive $250 in credit towards your next payment
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup'>
+                        Get Started { hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
@@ -42,4 +53,44 @@ height: 100%;
 -o-object-fit: cover;
 object-fit: cover;
 background: #232a34;
+`
+
+const HeroContent = styled.div`
+z-index: 3;
+max-width: 1200px;
+position: absolute;
+padding: 8px 24px;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const HeroH1 = styled.h1`
+color: #fff;
+font-size: 48px;
+text-align: center;
+
+@media screen and (max-width: 768px) {
+    font-size: 40px;
+}
+
+@media screen and (max-width: 480px) {
+    font-size: 32px;
+}
+`
+
+const HeroP = styled.p`
+margin-top: 24px;
+color: #fff;
+font-size: 24px;
+text-align: center;
+max-width: 600px;
+
+@media screen and (max-width: 768px) {
+    font-size: 24px;
+}
+
+@media screen and (max-width: 480px) {
+    font-size: 18px;
+}
 `
